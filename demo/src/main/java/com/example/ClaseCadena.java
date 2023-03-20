29d8f0e7927c84da49d586ce6e80be4c1f21c702
LuisMi01 <lmurbezv@gmail.com>
2023-03-20 18:42:32 +0100

    cambios

--- demo/src/main/java/com/example/ClaseCadena.java
+++ demo/src/main/java/com/example/ClaseCadena.java
@@ -1,13 +1,22 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class ClaseCadena {
     public void DevuelveIniciales(){
-        String nombre = "Juan Perez";
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce tu nombre y apellidos: ");
+        String nombre = teclado.nextLine();
+
+        
         String[] palabras = nombre.split(" ");
         String iniciales = "";
         for (String palabra : palabras) {
             iniciales += palabra.charAt(0);
         }
         System.out.println(iniciales);
+
+        teclado.close();
+
     }
 }
--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -5,6 +5,5 @@ public class Main
     public static void main( String[] args ){
         ClaseCadena cadena = new ClaseCadena();
         cadena.DevuelveIniciales();
-        
     }
 }
