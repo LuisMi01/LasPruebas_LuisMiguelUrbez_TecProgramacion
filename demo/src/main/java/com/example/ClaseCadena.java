6cb9b8f710532a37ea23b074511622a67ff563db
LuisMi01 <lmurbezv@gmail.com>
2023-03-25 10:28:59 +0100

    Trabajo terminado

--- demo/src/main/java/com/example/ClaseCadena.java
+++ demo/src/main/java/com/example/ClaseCadena.java
@@ -4,14 +4,15 @@ import java.util.StringTokenizer;
 
 public class ClaseCadena {
     public Object DevuelveIniciales(String nombre) {
+        //Para comprobar que el codigo funciona hay que realizar test en la carpeta test.java.com.example
 
         StringTokenizer st = new StringTokenizer(nombre);
         String iniciales = "";
         if (nombre.charAt(0) >= 'A' && nombre.charAt(0) <= 'Z'){
 
-        while (st.hasMoreTokens()) {
+            while (st.hasMoreTokens()) {
                 iniciales += st.nextToken().charAt(0);
-        }
+            }
         }
         System.out.println("Las iniciales son: " + iniciales);
         return iniciales;
