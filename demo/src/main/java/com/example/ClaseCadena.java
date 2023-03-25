6e284b053b3ffa07e372c69c56dbd04c029a8e1e
LuisMi01 <lmurbezv@gmail.com>
2023-03-25 10:21:18 +0100

    terminando tests

--- demo/src/main/java/com/example/ClaseCadena.java
+++ demo/src/main/java/com/example/ClaseCadena.java
@@ -1,18 +1,30 @@
 package com.example;
 
-public class ClaseCadena {
-    public Object DevuelveIniciales(String nombre){
+import java.util.StringTokenizer;
 
-        String[] palabras = nombre.split(" ");
+public class ClaseCadena {
+    public Object DevuelveIniciales(String nombre) {
+        /*String[] palabras = nombre.split(" ");
 
         String iniciales = "";
 
         for (int i = 0; i < nombre.length(); i++) {
-            if ( nombre.charAt(i) >= 'A' && nombre.charAt(i) <= 'Z' ) {
-                    iniciales += nombre.charAt(i);
+            if (nombre.charAt(i) >= 'A' && nombre.charAt(i) <= 'Z') {
+                iniciales += nombre.charAt(i);
             }
         }
         System.out.println("Las iniciales son: " + iniciales);
+        return iniciales;*/
+
+        StringTokenizer st = new StringTokenizer(nombre);
+        String iniciales = "";
+        while (st.hasMoreTokens()) {
+                iniciales += st.nextToken().charAt(0);
+
+        }
+        System.out.println("Las iniciales son: " + iniciales);
         return iniciales;
+
     }
+
 }
